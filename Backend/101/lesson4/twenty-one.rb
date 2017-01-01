38e1c56b0f678009fd737cab16ae031a2395f39a
Sonia Lin <sonia_lin@hotmail.com>
2016-12-31 23:58:12 -0600

    Added bonus features for 21 and cleaned up output messages

--- Backend/101/lesson4/twenty-one.rb
+++ Backend/101/lesson4/twenty-one.rb
@@ -1,10 +1,19 @@
 SUITS = ['diamond', 'spade', 'heart', 'club']
 FACE_VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
+MAX_HAND_VALUE = 31
+MIN_DEALER_HAND_VALUE = 27
+@dealer_score = 0
+@player_score = 0
+@round_number = 0
 
 def init_game
   @computer_hand = []
   @player_hand = []
   @deck = []
+  @round_number += 1
+  @has_round_winner = false
+  @round_winner = nil
+  puts "======== Welcome to #{MAX_HAND_VALUE}: ROUND #{@round_number} ========"
 end
 
 def init_deck
@@ -46,7 +55,7 @@ def calculate_hand_value(hand)
 
     loop do
       final_value = 11 * num_of_aces_valued_11 + 1 * num_of_aces_valued_1 + value_without_aces
-      break if final_value <= 21 || num_of_aces_valued_1 == num_of_aces
+      break if final_value <= MAX_HAND_VALUE || num_of_aces_valued_1 == num_of_aces
       num_of_aces_valued_11 -= 1
       num_of_aces_valued_1 += 1
     end
@@ -65,38 +74,35 @@ def get_hand_value(role)
 end
 
 def show_hand
-  require 'active_support/inflector'
-  num_of_unknown_dealer_cards = @computer_hand.size - 1
-  puts "Dealer has: " + @computer_hand.first[1].to_s + " and " + num_of_unknown_dealer_cards.to_s + " unknown " + "card".pluralize(num_of_unknown_dealer_cards)
-  player_string = ""
-  @player_hand.each do |card|
-    player_string << card[1].to_s
-    if card != @player_hand.last
-      player_string << " and "
-    end
-  end
-  puts "You have: " + player_string
+  print "Dealer has: " + @computer_hand.first.to_s
+  (@computer_hand.size - 1 ).times {print '[?, ?]'}
+  puts ""
+  puts "You have: " + @player_hand.to_s
 end
 
 def show_full_hand
+  puts "====SHOW HAND: ROUND #{@round_number}===="
   puts "Dealer's hand is #{@computer_hand}"
   puts "Your hand is #{@player_hand}"
-
-  puts "Dealer's hand's value is #{get_hand_value('computer')}"
-  puts "Your hand's value is #{get_hand_value('player')}"
 end
 
 loop do
-  puts "======Welcome to 21======"
   init_game
   init_deck
+
   2.times do
     deal('computer')
     deal('player')
   end
 
+  puts "Your turn."
   loop do
-    break if get_hand_value('player') > 21
+    if get_hand_value('player') > MAX_HAND_VALUE
+      @has_round_winner = true
+      @round_winner = 'dealer'
+      puts "BOO! YOU BUSTED."
+      break 
+    end
 
     show_hand
     puts "Hit? (y/n)"
@@ -105,30 +111,71 @@ loop do
     deal('player')
   end
 
-  loop do
-    break if get_hand_value('player') > 21 || get_hand_value('computer') >= 17
-    show_hand
-    puts "Dealer hits."
-    deal('computer')
+  
+  if @has_round_winner == false
+    puts "Dealer's turn."
+    loop do
+      if get_hand_value('computer') > MAX_HAND_VALUE
+        @has_round_winner = true
+        @round_winner = 'player'
+        puts "HA! DEALER BUSTED."
+        break
+      elsif get_hand_value('computer') >= MIN_DEALER_HAND_VALUE
+        show_hand
+        puts "Dealer stays."
+        break
+      else
+        show_hand
+        puts "Dealer hits."
+        deal('computer')  
+      end
+    end
   end
 
   show_full_hand
 
-  if get_hand_value('player') > 21
-    puts "BOO! YOU BUSTED."
-    puts "DEALER WINS."
-  elsif get_hand_value('computer') > 21
-    puts "HA! DEALER BUSTED."
-    puts "YOU WIN."
-  elsif get_hand_value('computer') > get_hand_value('player')
-    puts "DEALER WINS."
-  elsif get_hand_value('computer') == get_hand_value('player')
+  dealer_hand_value = get_hand_value('computer')
+  player_hand_value = get_hand_value('player')
+
+  puts "Dealer's hand's value is #{dealer_hand_value}"
+  puts "Your hand's value is #{player_hand_value}"
+
+  if @has_round_winner
+    if @round_winner == 'dealer'
+      puts "DEALER WINS THIS ROUND."
+      @dealer_score += 1
+    else
+      puts "YOU WIN THIS ROUND."
+      @player_score += 1
+    end
+  elsif dealer_hand_value > player_hand_value
+    puts "DEALER WINS THIS ROUND."
+    @dealer_score += 1
+  elsif dealer_hand_value == player_hand_value
     puts "IT'S A TIE."
   else
-    puts "CONGRATS! YOU WIN."
+    puts "CONGRATS! YOU WIN THIS ROUND."
+    @player_score += 1
   end
 
-  puts "Play again? (y/n)"
-  play_again = gets.chomp
+  puts "                             "
+  puts "-----------------------------"
+  puts "|    YOU    |     DEALER    |"
+  puts "|---------------------------|"
+  puts "|     #{@player_score}     |        #{@dealer_score}      |"
+  puts "-----------------------------"
+  puts "                             "
+
+  if @dealer_score == 5
+    puts "THE DEALER WINS! GAME OVER."
+    play_again = 'n'
+  elsif @player_score == 5
+    puts "YOU WIN THE GAME! CONGRATS."
+    play_again = 'n'
+  else
+    puts "Play again? (y/n)"
+    play_again = gets.chomp
+  end
+
   break if play_again == 'n'
 end
